7103fd9c5cee63b8ddd4027a5f1550e364101188
sagar manghnani <sagarmanghnani@gmail.com>
2018-10-20 17:24:50 +0530

    Added expense details to state object

--- src/App.js
+++ src/App.js
@@ -3,11 +3,47 @@ import Expenselist from './components/Expenselist';
 import './App.css';
 
 class App extends Component {
- 
+ constructor()
+ {
+   super()
+   this.state = {
+     expenselist:[]
+   }
+   this.addExpense = this.addExpense.bind(this);
+   this.checkState = this.checkState.bind(this);
+}
+
+
+addExpense()
+{ 
+  this.title = document.getElementById("title").value;
+  this.amount = document.getElementById("expenseMoney").value;
+  this.expenseDate = document.getElementById("expenseDate").value;
+  this.expenseObject = {
+    title: this.title,
+    amount: this.amount,
+    date: this.expenseDate
+  }
+  console.log(this.expenseObject);
+  this.setState({
+    expenselist: [...this.state.expenselist, this.expenseObject]
+  });
+}
+
+
+checkState()
+{
+  // this.setState({
+  //   expenselist: [...this.state.expenselist, "hello"]
+  // })
+  console.log(this.state.expenselist);
+}
+
   render() {
     return (
       <div className="App">
-        <Expenselist/>
+        <Expenselist getDetails = {this.addExpense}/>
+        <button onClick = {() => {this.checkState()}} >check</button>
       </div>
     );
   }
--- src/components/Expenselist.js
+++ src/components/Expenselist.js
@@ -5,25 +5,31 @@ export class Expenselist extends Component {
   static propTypes = {
 
   }
-  
+
+  provideDetails()
+  {
+
+  }
+
   render() {
     return (
       <div className = "expenseList">
         <div className = "title">
           <label>Add Title</label>
-          <input type = "text" name = "title"/>
+          <input type = "text"  id="title"/>
         </div>
 
         <div className = "expenseAmount">
           <label>Add Expense money</label>
-          <input type = "number" name = "expenseMoney"/>
+          <input type = "number" id = "expenseMoney"/>
         </div>
 
         <div className = "expenseCreatedDate">
           <label>Add Created Date</label>
-          <input type = "date" name = "expenseDate"/>
+          <input type = "date" id = "expenseDate"/>
         </div>
         
+        <button onClick = {() => this.props.getDetails()}>Submit</button>
       </div>
     )
   }
